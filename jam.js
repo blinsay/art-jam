a5c8b5b263317a671067bc819d61dd4f63edec5c
Sean Rankine <sean.rankine@me.com>
2018-03-12 18:15:30 -0400

    Add sliders

--- jam.js
+++ jam.js
@@ -18,15 +18,15 @@ let mapCapture = (capture, f) => {
 // pixel -> vector functions
 
 let _red = (x, y, r, g, b, a) => {
-  return { x: x, y: y, angle: r / 255, length: 1 };
+  return { x: x, y: y, angle: r / 255, length: 1};
 };
 
 let _green = (x, y, r, g, b, a) => {
-  return { x: x, y: y, angle: g / 255, length: 1 };
+  return { x: x, y: y, angle: g / 255, length: 1};
 };
 
 let _blue = (x, y, r, g, b, a) => {
-  return { x: x, y: y, angle: b / 255, length: 1 };
+  return { x: x, y: y, angle: b / 255, length: 1};
 };
 
 let scaleVec = (s) => (v) => {
@@ -34,6 +34,7 @@ let scaleVec = (s) => (v) => {
   return v;
 };
 
+//intensity function (0.299*r + 0.587*g + 0.114*b)
 // drawing functions
 
 let vectorAsLine = (scale, rotate) => (v) => {
@@ -50,22 +51,35 @@ let vectorAsLine = (scale, rotate) => (v) => {
 var capture;
 
 function setup() {
+  let outputSize = 60
   createCanvas(windowWidth, windowHeight);
   capture = createCapture(VIDEO);
-  capture.size(60, 60);
+  capture.size(outputSize, outputSize);
+
+  // create sliders
+  lengthSlider = createSlider(0, 50, 10);
+  lengthSlider.position(outputSize * 11, 20);
+  delaySlider = createSlider(0, 255, 255, 1);
+  delaySlider.position(outputSize * 11, 40);
+  rotateSlider = createSlider(100, 2000, 2000);
+  rotateSlider.position(outputSize * 11, 60);
 }
 
 function draw() {
-  background(255, 255, 255, 255);
+  var lineLength = lengthSlider.value();
+  var delay = delaySlider.value();
+  var rotate = rotateSlider.value() < 2000 ? millis() / rotateSlider.value() : HALF_PI;
+
+  background(255, 255, 255, delay);
   stroke(0, 0, 0, 150);
   strokeWeight(1);
 
   let colorVectors = (x, y, r, g, b, a) => [
     _red(x, y, r, g, b, a),
     _green(x, y, r, g, b, a),
-    _blue(x, y, r, g, b, a),
+    _blue(x, y, r, g, b, a)
   ];
-  let drawVec = vectorAsLine(10, HALF_PI);
+  let drawVec = vectorAsLine(10, rotate);
   capture.loadPixels();
-  mapCapture(capture, colorVectors).forEach((vecs) => vecs.map(scaleVec(5)).forEach(drawVec));
+  mapCapture(capture, colorVectors).forEach((vecs) => vecs.map(scaleVec(lineLength)).forEach(drawVec));
 }
